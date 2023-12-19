e39dfd28edd63c6187c6ac16b28c65e14c34a5c7
Dan Lock <codenil@proton.me>
2023-12-19 23:07:58 +0000

    Add word processing

--- src/build.rs
+++ src/build.rs
@@ -1,5 +1,15 @@
+use crate::build_utils::{subtlex, wordnet};
+use anyhow::Result;
 use rsass::{compile_scss_path, output::Format};
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{BufWriter, Write},
+    string::ToString,
+};
 use std::{fs, io, path::Path};
+
+mod build_utils;
 
 fn main() -> io::Result<()> {
     let scss_path = Path::new("src").join("style.scss");
@@ -8,5 +18,60 @@ fn main() -> io::Result<()> {
     let output_path = Path::new("assets").join("style.css");
     fs::write(output_path, css)?;
 
+    if !Path::new("src/words.txt").exists() {
+        if let Err(result) = parse_words() {
+            println!("cargo:warning=Error parsing words: {result:?}");
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct WordSets {
+    pub easy_words: HashSet<String>,
+    pub medium_words: HashSet<String>,
+    pub hard_words: HashSet<String>,
+}
+
+fn parse_words() -> Result<()> {
+    let start_time = std::time::Instant::now();
+    let subtlex_result: WordSets = subtlex::parse().expect("Error parsing subtlex");
+    let wordnet_result: HashSet<String> = wordnet::parse().expect("Error parsing wordnet");
+    println!("cargo:warning=Parse time: {:?}", start_time.elapsed());
+
+    // Gather words that are in both lists
+    let word_sets = WordSets {
+        easy_words: subtlex_result.easy_words.intersection(&wordnet_result).cloned().collect(),
+        medium_words: subtlex_result.medium_words.intersection(&wordnet_result).cloned().collect(),
+        hard_words: subtlex_result.hard_words.intersection(&wordnet_result).cloned().collect(),
+    };
+    println!("cargo:warning=Easy words count: {:?}", word_sets.easy_words.len());
+    println!("cargo:warning=Medium words count: {:?}", word_sets.medium_words.len());
+    println!("cargo:warning=Hard words count: {:?}", word_sets.hard_words.len());
+
+    println!("cargo:warning=Intersection time: {:?}", start_time.elapsed());
+
+    let file_path = "src/words.txt";
+    let mut file = BufWriter::new(File::create(file_path)?);
+
+    write_word_sets(&mut file, &word_sets)?;
+
+    file.flush()?;
+    println!("cargo:warning=Total time: {:?}", start_time.elapsed());
+    Ok(())
+}
+
+fn write_word_sets(file: &mut BufWriter<File>, word_sets: &WordSets) -> Result<()> {
+    write_word_set(file, "easy_words", &word_sets.easy_words)?;
+    write_word_set(file, "medium_words", &word_sets.medium_words)?;
+    write_word_set(file, "hard_words", &word_sets.hard_words)?;
+    Ok(())
+}
+
+fn write_word_set(file: &mut BufWriter<File>, title: &str, words: &HashSet<String>) -> Result<()> {
+    writeln!(file, "[{title}]")?;
+    let words_string = words.iter().map(ToString::to_string).collect::<Vec<String>>().join(",");
+    writeln!(file, "{words_string}")?;
     Ok(())
 }
--- todo.md
+++ todo.md
@@ -3,3 +3,19 @@ When item is guessed, pop out the coins on the display and have them float into 
 Background music
 Start sound isnt great
 Clear question box on submit
+Add chat
+
+Quizmaster rewrite:
+
+-   [x] Add lobby settings page for key player to edit, including:
+    -   [x] Number of items total, default to 10 and game ends once all items used up
+    -   [x] Item selection options
+        -   [x] Item difficulty, picks less known words
+        -   [x] Item categories to filter from
+    -   [x] Quizmaster mode, where host picks the items and answers questions
+        -   [x] Refresh button which refreshes all items in list, and individually per item
+        -   [x] Edit items in list for custom item submissions
+        -   [x] When user asks question it adds to answer queue for host to answer (requires unknown answer choice)
+
+SUBTLEX-UK database of word frequencies https://www.tandfonline.com/doi/suppl/10.1080/17470218.2013.850521
+WordNet database of word categories https://wordnet.princeton.edu/download/current-version
